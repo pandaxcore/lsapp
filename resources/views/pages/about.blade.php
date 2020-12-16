b1e8729be56c6f54b540009ed05ade3847d54787
pandaxcore <67134481+pandaxcore@users.noreply.github.com>
2020-12-17 01:15:25 +0600

    Revert "Pages add"

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'LSAPP')}}</title>
-
-    </head>
-    <body>
-        <h2>About</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sed eaque nostrum culpa architecto totam <br>
-        eveniet eos maxime quod ad, in laborum ratione labore a doloremque. Dolorum magni iure ullam!</p>
-    </body>
-</html>
+<h3>About</h3>
