0cfa70be0c08a9e2d769b92b6ee1f9869ebea20a
pandadesign7@gmail.com <pandaXcore>
2020-12-21 16:16:04 +0600

    changes some changes

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'LSAPP')}}</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    </head>
-    <body>
-        <h2>Services Page</h2>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores libero numquam reiciendis excepturi, <br>
-            itaque quasi quos temporibus placeat distinctio quidem molestias nobis sed ipsum sunt incidunt odio est voluptate unde.
-        </p>
-    </body>
-</html>
+@extends('layouts/app')
+
+@section('content')
+    <h2>Services Page</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores libero numquam reiciendis excepturi, <br>
+        itaque quasi quos temporibus placeat distinctio quidem molestias nobis sed ipsum sunt incidunt odio est voluptate unde.
+    </p>
+@endsection
