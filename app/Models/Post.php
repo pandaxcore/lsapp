64e14bb22726f4280f154e331a34d29b62f9b4da
pandaxcore <pandadesign7@gmail.com>
2021-01-19 17:57:39 +0600

    updations

---
+++ app/Models/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    //table name
+    protected $table = 'posts';
+    //primary key
+    protected $primaryKey = 'id';
+    //timestamps
+    public $timestamps = true;
+}
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts/app')
 
 @section('content')
+<div class="jumbotron text-center">
     <h2>{{$title}}</h2>
     <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores libero numquam reiciendis excepturi, <br>
         itaque quasi quos temporibus placeat distinctio quidem molestias nobis sed ipsum sunt incidunt odio est voluptate unde.
     </p>
+    <p>
+        <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
+        <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
+    </p>
+</div>
 @endsection
