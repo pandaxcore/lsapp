313b85ae0c0f1b466152a15503605be0c9753f58
pandaxcore <pandadesign7@gmail.com>
2021-02-04 17:21:33 +0600

    adding create form

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Ninth navbar example">
+  <div class="container-xl">
+    <a class="navbar-brand" href="/">{{config('app/name', 'LSAPP')}}</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExample07XL">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/services">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/posts">Blog</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts/app')
+
+@section('content')
+    <h1>Create post</h1>
+
+    {!! Form::open(['action' => 'App\Http\Controllers\PostsController@store', 'method' => 'POST']) !!}
+        <div class="form-group">
+            {{Form::label('title', 'Tilte')}}
+            {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('body', 'Body')}}
+            {{Form::text('body', '', ['class' => 'form-control', 'placeholder' => 'Body Text'])}}
+        </div>
+    {!! Form::close() !!}
+    
+@endsection
